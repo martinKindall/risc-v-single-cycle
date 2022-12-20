81e93f107bfbbf4aaa37a5623bbf65dd932fa288
Martin Cornejo <mart256@gmail.com>
2022-12-20 22:35:12 +0100

    Added logic to decode instructions and get all possible immediate formats.

--- sources_1/new/Decoder.sv
+++ sources_1/new/Decoder.sv
@@ -1,26 +1,33 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/20/2022 10:17:28 PM
-// Design Name: 
-// Module Name: Decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Decoder(
+    input logic [31:0] inst
+);
 
-    );
+    localparam len = 6;
+
+    logic isALUreg =  (instr[len:0] == 7'b0110011); // rd <- rs1 OP rs2   
+    logic isALUimm =  (instr[len:0] == 7'b0010011); // rd <- rs1 OP Iimm
+    logic isBranch =  (instr[len:0] == 7'b1100011); // if(rs1 OP rs2) PC<-PC+Bimm
+    logic isJALR   =  (instr[len:0] == 7'b1100111); // rd <- PC+4; PC<-rs1+Iimm
+    logic isJAL    =  (instr[len:0] == 7'b1101111); // rd <- PC+4; PC<-PC+Jimm
+    logic isAUIPC  =  (instr[len:0] == 7'b0010111); // rd <- PC + Uimm
+    logic isLUI    =  (instr[len:0] == 7'b0110111); // rd <- Uimm   
+    logic isLoad   =  (instr[len:0] == 7'b0000011); // rd <- mem[rs1+Iimm]
+    logic isStore  =  (instr[len:0] == 7'b0100011); // mem[rs1+Simm] <- rs2
+    logic isSYSTEM =  (instr[len:0] == 7'b1110011); // special
+
+    logic [4:0] rs1Id = instr[19:15];
+    logic [4:0] rs2Id = instr[24:20];
+    logic [4:0] rdId  = instr[11:7];
+
+    logic [2:0] funct3 = instr[14:12];
+    logic [6:0] funct7 = instr[31:25];
+
+    logic [31:0] Uimm = {    instr[31],   instr[30:12], {12{1'b0}}};
+    logic [31:0] Iimm = {{21{instr[31]}}, instr[30:20]};
+    logic [31:0] Simm = {{21{instr[31]}}, instr[30:25],instr[11:7]};
+    logic [31:0] Bimm = {{20{instr[31]}}, instr[7],instr[30:25],instr[11:8],1'b0};
+    logic [31:0] Jimm = {{12{instr[31]}}, instr[19:12],instr[20],instr[30:21],1'b0};
+
 endmodule
