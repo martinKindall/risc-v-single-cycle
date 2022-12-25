b95fe2aa3ec64943fce75c19e20989a6effb3cb0
Martin Cornejo <mart256@gmail.com>
2022-12-25 13:50:27 +0100

    Added instructions to turn leds on using io driver

--- sources_1/new/IODriver.sv
+++ sources_1/new/IODriver.sv
@@ -6,11 +6,13 @@ module IODriver(
     output logic [4:0] leds
 );
     localparam IO_LEDS_bit = 0;
+    logic [29:0] wordAddr;
 
     always_ff @(posedge clk)
-        if(isIO & addr[IO_LEDS_bit])
+        if(isIO & wordAddr[IO_LEDS_bit])
             leds <= memWdata[4:0];
 
     assign isIO = addr[22];
+    assign wordAddr = addr[31:2];
 
 endmodule
